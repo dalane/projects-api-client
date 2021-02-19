eae6535285fa7bb2cd0b15a5ef71b9b25785af3c
Dallas Ewing <dallas.ewing@dalane.co.uk>
2021-02-19 10:43:42 +0000

    added default custom user-agent header

--- src/adapter.ts
+++ src/adapter.ts
@@ -4,16 +4,28 @@ export { Components } from '../schema/openapi';
 
 export type ProjectsApiClient = Client;
 
+// @ts-ignore
+import { version } from '../package.json';
+
 export interface ApiClientSettings {
 	/**
-	 * URL to the API base including the version, default. "https: //projects.api.dalane.cloud/v1"
+	 * OPTIONAL URL to the API base including the version, default to
+	 * "https://projects.api.dalane.cloud/v1"
 	 */
 	server?: { url: string; description?: string };
+	/**
+	 * OPTIONAL An identifier of the service using the client. It is added to the
+	 * user-agent header.
+	 */
+	identifier?: string;
 }
 
 export async function createClient(options?: ApiClientSettings): Promise<ProjectsApiClient> {
-	const { server } = options ?? {};
+	const { server, identifier } = options ?? {};
 	const api = new OpenAPIClient({ definition: '../schema/openapi.json', withServer: server ?? 'live' });
 	const client = await api.getClient<ProjectsApiClient>();
+	client.defaults.headers = {
+		'user-agent': `@dalane/projects-api-client:${version}${!!identifier ? `;${identifier}` : ''}`
+	};
 	return client;
 }
